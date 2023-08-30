db262fb1240cbbe10a6d05161a88973734629252
Hiddenfeet <134339758+Hiddenfeet@users.noreply.github.com>
2023-08-30 22:04:14 +0200

    1

--- nft_raffle_app/src/main.tsx
+++ nft_raffle_app/src/main.tsx
@@ -11,15 +11,28 @@ import './index.css'
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = 'mumbai'
+const activeChain = 'CronosBeta'
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
 
 root.render(
   <ThirdwebProvider
-    clientId={import.meta.env.VITE_TEMPLATE_CLIENT_ID}
-    activeChain={activeChain}
+    
+    activeChain={{
+      chainId: 25,
+      rpc: ["https://mainnet.cronoslabs.com/v1/55e37d8975113ae7a44603ef8ce460aa"],
+      nativeCurrency: {
+        decimals: 18,
+        name: "Cronos",
+        symbol: "CRO",
+      },
+      shortName: "CRO",
+      slug: "Cronos",
+      testnet: false,
+      chain: "Cronos Mainnet",
+      name: "Cronos Mainnet",
+    }}
     supportedWallets={[metamaskWallet(), walletConnect()]}
   >
     <NextUIProvider>
